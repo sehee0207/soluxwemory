76eb43435fe8bf9f37725c3ad836b42ef0cf10d3
김세희 <lolry0207@gmail.com>
2023-07-05 23:03:59 +0900

    add Calendar

--- src/component/ui/Calender.jsx
+++ src/component/ui/Calender.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import '../../style/Calendar.css';
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -25,13 +28,28 @@ const StyledComname = styled.div`
     margin: 0px 15px;
 `
 
+/* https://velog.io/@pikadev1771/react-calendar-%EC%BB%A4%EC%8A%A4%ED%85%80%ED%95%98%EA%B8%B0-%EB%82%A0%EC%A7%9C-%EB%B3%80%ED%99%98-%ED%98%84%EC%9E%AC-%EB%8B%AC-%EA%B5%AC%ED%95%98%EA%B8%B0-%EC%BD%98%ED%85%90%EC%B8%A0-%EC%B6%94%EA%B0%80%ED%95%98%EA%B8%B0*/
+
 function Calender(props){
     const { comname } = props;
+    const [ value, onChange ] = useState(new Date());
 
     return(
         <Wrapper>
             <StyledComname>{comname}</StyledComname>
-            <Container/>
+            <Container>
+                <Calendar
+                    locale="en"
+                    calendarType="US"
+                    onChange={onChange}
+                    value={value}
+                    next2Label={null}
+                    prev2Label={null}
+                />
+                <div className="text-gray-500 mt-4">
+                    {moment(value).format("YYYY년 MM월 DD일")} 
+                </div> 
+            </Container>
         </Wrapper>
         
     )
